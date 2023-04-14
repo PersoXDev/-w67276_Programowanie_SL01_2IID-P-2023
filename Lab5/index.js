fd2015c7c1caea85ae3b8437fe8dc3bfd8b832a6
Krystian Tłuczek <w67276@student.wsiz.edu.pl>
2023-04-14 15:02:02 +0200

    Lab5

---
+++ Lab5/index.js
@@ -0,0 +1,14 @@
+// Zadanie 1
+let get_variables = prompt("Podaj wiek: ")
+console.log(get_variables)
+if (get_variables >=18){
+    window.alert("pełnoletni")
+}
+else{
+    window.alert("niepełnoletni")
+}
+
+// Zadanie2
+let temperature=prompt("Podaj temperature w celsjuszu: ")
+let farenheit = temperature*2+32
+console.log(farenheit)
